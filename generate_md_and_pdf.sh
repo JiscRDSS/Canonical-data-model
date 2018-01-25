5d8bb068b1080ad38494c3000e965be95447180e
Michal Charemza <michal@charemza.name>
2018-01-25 17:10:42 +0000

    [RDSS-1861] Run pre-commit on generated files to fix end of files
    This is since by default the generated files make pre-commit fail, which means,
    if the files are committed without pre-commit changing the files, on the _next_
    PR, the build will fail because pre-commit is run before anything else.
    
    Suspect not doing this in the earlier version of the automatic generation
    script was a contributing factor to merge conflicts.

--- generate_md_and_pdf.sh
+++ generate_md_and_pdf.sh
@@ -22,3 +22,9 @@ echo "Generating concept model markdown and PDF"
 mdgen render -m Data-Model/Background/concept-model.mdj -t main-template.ejs -o "markdown/concept-model/<%=element.name%>.md" -s @UMLClass
 mdgen pdf -m Data-Model/Background/concept-model.mdj -o export_pdf/concept-model.pdf -s @Diagram -z A4 -l landscape -n yes
 echo "Generated"
+
+echo "Running pre-commit to make sure any extra lines are removed"
+# pre-commit changes files, but also returns an error code
+set +e
+pre-commit run --all-files --verbose
+set -e
